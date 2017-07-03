dba0384a3ccf8cd7dd43b2f0c2cf9f0f01770b4f
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-07-03 16:33:27 +0200

    ajout d'un compteur de points

--- js/main.js
+++ js/main.js
@@ -2,26 +2,37 @@ var tab = ["pierre", "feuille", "ciseaux"];
 var ordi = tab[parseInt(Math.random()*tab.length)];
 var choice = prompt("pierre, feuille ou ciseaux?");
 var score = "";
+var compteurOrdi = 0;
+var compteurChoice = 0;
 
 function game(){
-  //for (i=0; i<; i++){
+
     if (ordi === choice){
       alert("égalité");
+      alert(compteurOrdi + "-" + compteurChoice)
     }
    else if(ordi === tab[0] && choice === "feuille"){
      alert ("Vous avez gagné");
+     compteurChoice ++;
+     alert(compteurOrdi + "-" + compteurChoice)
    }
    else if (ordi === tab[1] && choice === "ciseaux"){
      alert ("Vous avez gagné");
+     compteurChoice ++;
+     alert(compteurOrdi + "-" + compteurChoice)
    }
    else if (ordi === tab[2] && choice === "pierre"){
     alert ("Vous avez gagné");
+    compteurChoice ++;
+    alert(compteurOrdi + "-" + compteurChoice)
    }
    else{
     alert ("Vous avez perdu");
+    compteurOrdi ++;
+    alert(compteurOrdi + "-" + compteurChoice)
    }
   }
-  return score += "Manche fini";
-//}
+ // return score += compteurOrdi + "-" + compteurChoice + " " +"Manche fini";
+}
 
 game();
